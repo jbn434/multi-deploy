2fabd3cc210dee25931a9039f61a455eab3a0fe6
jbn <jbn434@gmail.com>
2025-08-02 14:15:22 +0530

    Update backend.tf

--- infra/s3/backend.tf
+++ infra/s3/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bckt4321"      
+    bucket         = "idlms-stage-built-artifact"      
     key            = "dev/s3/terraform.tfstate"          
     region         = "eu-west-1"
     encrypt        = true
